fce1f2ec5bfe678a0cdfc685863c2b6e8463415d
sayantanbanerjee <sayantanbanerjee@localhost>
2020-10-15 15:29:23 +0530

    new variable

--- EmployeeManagement/src/com/lti/model/Project.java
+++ EmployeeManagement/src/com/lti/model/Project.java
@@ -3,6 +3,7 @@ package com.lti.model;
 public class Project {
 	private int projectCode;
 	private String projectName;
+	private String projectType;
 	
 	public int getProjectCode() {
 		return projectCode;
@@ -15,6 +16,12 @@ public class Project {
 	}
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
+	}
+	public String getProjectType() {
+		return projectType;
+	}
+	public void setProjectType(String projectType) {
+		this.projectType = projectType;
 	}
 	
 	
